c53a855494a5fa050ffb0a59cd8837c64ab378ee
dean <spencerdberg1@gmail.com>
2018-12-02 14:05:32 -0600

    <tl;dr>

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -2,14 +2,12 @@ import React from "react";
 
 const Modal = props => (
     <div id={props.modalID} className="modal">
-    <div className="modal-content">
-      <h4>{props.title}</h4>
-      {props.children}
+      <div className="modal-content">
+        <h4>{props.title}</h4>
+        {props.children}
+        <a id="close" href="#!" className="modal-close waves-effect waves-green btn-flat">Close</a>
+      </div>
     </div>
-    <div className="modal-footer">
-      <a href="#!" className="modal-close waves-effect waves-green btn-flat">Close</a>
-    </div>
-  </div>
   );
   
   export default Modal;
